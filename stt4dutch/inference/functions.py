a0956acbfe70c51d160a4b25b649d2b3ab30879c
Willem Röpke <willem.ropke@vub.be>
2019-11-12 09:27:53 +0100

    Updates to selected models and the way the inference is executed

--- stt4dutch/inference/functions.py
+++ stt4dutch/inference/functions.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 audio_files_folder = 'inference/uploads/'
-models_folder = '/Users/willemropke/Documents/BT-projects/Models/'
+models_folder = '/Users/willemropke/Documents/BT-projects/Demo-models/'
 
 def handle_uploaded_file(f):  
     with open(audio_files_folder + f.name, 'wb+') as destination:  
@@ -13,7 +13,11 @@ def run_inference(model, audio):
     # The command that we need to execute to get the prediction
     audio_path = os.path.abspath(audio_files_folder + audio)
     model_path = os.path.abspath(models_folder + model)
-    command = "deepspeech --model " + model_path + " --alphabet alphabet-en.txt --lm lm.binary --trie trie --audio '" + audio_path + "'"
+    alphabet = "alphabet.txt"
+    if model.endswith("CF.pb") or model.endswith("DF.pb") or model.endswith("FF.pb"):
+        alphabet = "alphabet-en.txt"
+    print(model)
+    command = "deepspeech --model " + model_path + " --alphabet " + alphabet + " --lm lm.binary --trie trie --audio '" + audio_path + "'"
 
     # Get the output from the command and add it to the predictions
     output = subprocess.check_output(command, cwd='/Users/willemropke/Documents/BT-projects/Testing', shell=True)
